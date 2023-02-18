bfc7c41f2632a5ad7b7ba2030fc4a1197acbe200
edhepp <ed.hepplewhite@gmail.com>
2023-02-17 19:02:37 -0600

    Created enemy object pool

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -8,7 +8,7 @@ public class EnemyHealth : MonoBehaviour
     int currentHitPoints = 0;
 
 
-    void Start()
+    void OnEnable()
     {
         currentHitPoints = maxHitpoints;
     }
@@ -23,7 +23,7 @@ public class EnemyHealth : MonoBehaviour
         currentHitPoints--;
         if (currentHitPoints < 1)
         {
-            Destroy(this.gameObject);
+            gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -8,7 +8,7 @@ public class EnemyMover : MonoBehaviour
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
     [SerializeField] [Range(0f, 5f)] float speed = 1.0f;
 
-    void Start()
+    void OnEnable()
     {
         FindPath();
         ReturnToStart();
@@ -51,6 +51,6 @@ public class EnemyMover : MonoBehaviour
             }
         }
 
-        Destroy(this.gameObject);
+        gameObject.SetActive(false);
     }
 }
